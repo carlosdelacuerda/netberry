300a4684a559dc734573352af7e1c09f1c3efd36
Carlos de la Cuerda <carlosdelacuerda@gmail.com>
2021-03-15 13:11:58 +0100

    Creada estructura de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CardsComponent } from './components/cards/cards.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { FeaturesComponent } from './components/features/features.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CardsComponent,
+    CarouselComponent,
+    FeaturesComponent
   ],
   imports: [
     BrowserModule,
